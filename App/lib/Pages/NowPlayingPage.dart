13472d92cb1c8c2aec90296fbbe651854fbe5182
betsyninoska <concienciaenplenitud@gmail.com>
2024-10-21 20:25:49 -0400

    NowPlayingPage.dart

--- App/lib/Pages/NowPlayingPage.dart
+++ App/lib/Pages/NowPlayingPage.dart
@@ -1,24 +1,29 @@
 import 'package:flutter/material.dart';
 
+import 'LoginPage.dart';
 
 
-class LoginPage extends StatefulWidget {
+
+
+
+class NowPlayingPage extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() => LoginPageState();
+  State<StatefulWidget> createState() => NowPlayingPageState();
   
 }
 
 
-class LoginPageState extends State<LoginPage>{
+class NowPlayingPageState extends State<NowPlayingPage>{
   @override
   Widget build(BuildContext context) {
    return Scaffold(
 
       body:SafeArea(
         child: ElevatedButton(
-          child:Text("LoginPage"),
+          child:Text("Login"),
           onPressed:()=>{
-                Navigator.push(context,MaterialPageRoute(builder:(context)=>NowPlayingPage()));
+            Navigator.push(context,MaterialPageRoute(builder:(context)=>LoginPage()))
+
           },
         ),
       ),
